3013864ae116d982c335b824aaf29c67cccf7c6f
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-02-11 15:57:43 +0000

    EngineMain initial

---
+++ src/main/java/jp/mufg/api/EngineMain.java
@@ -0,0 +1,28 @@
+package jp.mufg.api;
+
+import jp.mufg.api.util.MetaData;
+import net.openhft.chronicle.Chronicle;
+import net.openhft.chronicle.ChronicleQueueBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class EngineMain {
+    static final String CHRONICLE_BASE = System.getProperty("chronicle", "engine-input");
+    static final String TARGET = System.getProperty("target", "target");
+    static final String CALCULATORS = System.getProperty("calculators");
+
+
+    public static void main(String... ignored) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Chronicle chronicle = ChronicleQueueBuilder.vanilla(CHRONICLE_BASE).build();
+        MetaData.setId(chronicle, (byte) 1);
+
+        DataMartEngine engine = new DataMartEngine();
+        for (String name : CALCULATORS.split(",")) {
+            Calculator calculator = (Calculator) Class.forName(name)
+                    .newInstance();
+            DirectDataMart dataMart = new FilteringDataMart(TARGET, new HashMap<>(), calculator);
+            engine.add(new ChronicleDataMart(chronicle, dataMart));
+        }
+    }
+}
