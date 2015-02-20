1858a58a982e8fe452acc950a05998cd8a9d1f6a
daschgit <dsc@dasch.dk>
2015-02-20 11:38:55 +0000

    Adding examples package, which currently holds classes to demonstrate how to work with Maps and Chronicle queue.

---
+++ src/main/java/jp/mufg/examples/ChronicleMapTester.java
@@ -0,0 +1,17 @@
+package jp.mufg.examples;
+
+import jp.mufg.api.*;
+
+import java.util.*;
+
+/**
+ * Created by daniels on 20/02/2015.
+ */
+public interface ChronicleMapTester
+{
+    void onMapMarketDataUpdate(MapMarketDataUpdate mapMarketDataUpdate);
+
+    void onMarketDataUpdateMapStringDouble(String someString, Map<String, Double> map);
+
+    void onMarketDataUpdateMapStringObject(String someString, Map<String, Object> map);
+}
---
+++ src/main/java/jp/mufg/examples/MapTestProgram.java
@@ -0,0 +1,243 @@
+package jp.mufg.examples;
+
+import jp.mufg.api.util.*;
+import net.openhft.chronicle.*;
+import net.openhft.chronicle.tools.*;
+import net.openhft.lang.model.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by daniels on 20/02/2015.
+ */
+public class MapTestProgram
+{
+    public static void main(String[] args)
+    {
+        //This fails because the DataValueClasses.newInstance cannot create a class which has map or collection. Is this by design?
+//        runGeneratedClassWithCollection();
+
+
+        //This fails due to StreamCorruptedException. I am guessing this is because it cannot parse values with different types (type of Object)
+        //Exception in thread "main" java.lang.IllegalStateException: java.lang.IllegalStateException: java.io.StreamCorruptedException: UTF length invalid 84 remaining: 22
+//        runMapWriteReadWithObject();
+
+
+        //This seems to work, presumably because values are of the same type?
+//        runMapWriteReadWithStrings();
+
+
+        //This seems to work, presumably because values are of the same type?
+//        runMapWriteReadWithDouble();
+
+
+        //This works just fine
+//        runMapAsMethodParamNotInObjectStringDouble();
+
+
+        //This works just fine
+        runMapAsMethodParamNotInObjectStringObject();
+    }
+
+    //This fails because the DataValueClasses.newInstance cannot create a class which has map or collection. Is this by design?
+    private static void runGeneratedClassWithCollection()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ChronicleMapTesterImpl chronicleMapTesterImpl = new ChronicleMapTesterImpl();
+
+            ChronicleMapTester writer = ToChronicle.of(ChronicleMapTester.class, chronicle);
+            FromChronicle<ChronicleMapTesterImpl> reader = FromChronicle.of(chronicleMapTesterImpl, chronicle.createTailer());
+
+            MapMarketDataUpdate mapMarketDataUpdate = DataValueClasses.newInstance(MapMarketDataUpdate.class);
+            mapMarketDataUpdate.setSource("S");
+            mapMarketDataUpdate.setExchange("E");
+            mapMarketDataUpdate.setInstrument("I");
+
+            //Put a MapMarketDataUpdate on the queue
+            writer.onMapMarketDataUpdate(mapMarketDataUpdate);
+
+            //This should read the MapMarketDataUpdate from the queue and call the implementation
+            reader.readOne();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //This fails due to StreamCorruptedException. I am guessing this is because it cannot parse values with different types (type of Object)
+    //Exception in thread "main" java.lang.IllegalStateException: java.lang.IllegalStateException: java.io.StreamCorruptedException: UTF length invalid 84 remaining: 22
+    private static void runMapWriteReadWithObject()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ExcerptAppender appender = chronicle.createAppender();
+            appender.startExcerpt();
+
+            Map<String, Object> putMap = new HashMap<String, Object>();
+            putMap.put("TestKey", "TestValue");
+            putMap.put("TestKey2", "sdsd");
+
+            appender.writeMap(putMap);
+
+            appender.finish();
+
+            ExcerptTailer tailer = chronicle.createTailer();
+
+            Map<String, Object> newMap = new HashMap<String, Object>();
+
+            if (tailer.nextIndex())
+            {
+
+                tailer.readMap(newMap, String.class, Object.class);
+
+                System.out.println("Map: " + newMap);
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //This seems to work, presumably because values are of the same type?
+    private static void runMapWriteReadWithStrings()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ExcerptAppender appender = chronicle.createAppender();
+            appender.startExcerpt();
+
+            Map<String, String> putMap = new HashMap<String, String>();
+            putMap.put("TestKey", "TestValue");
+            putMap.put("TestKey2", "sdsd");
+
+            appender.writeMap(putMap);
+
+            appender.finish();
+
+            ExcerptTailer tailer = chronicle.createTailer();
+
+            Map<String, String> newMap = new HashMap<String, String>();
+
+            if (tailer.nextIndex())
+            {
+
+                tailer.readMap(newMap, String.class, String.class);
+
+                System.out.println("Map: " + newMap);
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //This seems to work, presumably because values are of the same type?
+    private static void runMapWriteReadWithDouble()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ExcerptAppender appender = chronicle.createAppender();
+            appender.startExcerpt();
+
+            Map<String, Double> putMap = new HashMap<String, Double>();
+            putMap.put("TestKey", 1.0);
+            putMap.put("TestKey2", 2.0);
+
+            appender.writeMap(putMap);
+
+            appender.finish();
+
+            ExcerptTailer tailer = chronicle.createTailer();
+
+            Map<String, Double> newMap = new HashMap<String, Double>();
+
+            if (tailer.nextIndex())
+            {
+
+                tailer.readMap(newMap, String.class, Double.class);
+
+                System.out.println("Map: " + newMap);
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //This works just fine
+    private static void runMapAsMethodParamNotInObjectStringDouble()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ChronicleMapTesterImpl chronicleMapTesterImpl = new ChronicleMapTesterImpl();
+
+            ChronicleMapTester writer = ToChronicle.of(ChronicleMapTester.class, chronicle);
+            FromChronicle<ChronicleMapTesterImpl> reader = FromChronicle.of(chronicleMapTesterImpl, chronicle.createTailer());
+
+            //Call method that has a map as param
+            Map<String, Double> mapStringDouble = new HashMap<String, Double>();
+            mapStringDouble.put("Key1", 1.0);
+            mapStringDouble.put("Key2", 2.0);
+
+            writer.onMarketDataUpdateMapStringDouble("TestString - Map<String, Double>", mapStringDouble);
+
+            //This should read the onMarketDataUpdateMapStringDouble with string and map
+            reader.readOne();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //This works just fine
+    private static void runMapAsMethodParamNotInObjectStringObject()
+    {
+        try
+        {
+            String chronicleQueueBase = "C:\\LocalFolder\\Chronicle\\data";
+            Chronicle chronicle = ChronicleQueueBuilder.vanilla(chronicleQueueBase).build();
+            ChronicleTools.deleteDirOnExit(chronicleQueueBase);
+
+            ChronicleMapTesterImpl chronicleMapTesterImpl = new ChronicleMapTesterImpl();
+
+            ChronicleMapTester writer = ToChronicle.of(ChronicleMapTester.class, chronicle);
+            FromChronicle<ChronicleMapTesterImpl> reader = FromChronicle.of(chronicleMapTesterImpl, chronicle.createTailer());
+
+            //Call method that has a map as param
+            Map<String, Object> mapStringObject = new HashMap<String, Object>();
+            mapStringObject.put("KeyDouble", 1.0);
+            mapStringObject.put("KeyValue", "valueString");
+
+            writer.onMarketDataUpdateMapStringObject("TestString - Map<String, Double>", mapStringObject);
+
+            //This should read the onMarketDataUpdateMapStringDouble with string and map
+            reader.readOne();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
