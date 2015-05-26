573effb8fd0e99ccb12a1142b1958851b51398ba
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-05-26 19:06:34 +0100

    WIRE-3 Add tests to ensure values are written as expected.

--- src/test/java/jp/mufg/chronicle/map/eventlistener/MapEventListenerStatelessClientTest.java
+++ src/test/java/jp/mufg/chronicle/map/eventlistener/MapEventListenerStatelessClientTest.java
@@ -1,241 +1,231 @@
-//package jp.mufg.chronicle.map.eventlistener;
-//
-//import jp.mufg.*;
-//import net.openhft.chronicle.hash.replication.*;
-//import net.openhft.chronicle.map.*;
-//import net.openhft.chronicle.tools.*;
-//import net.openhft.lang.values.*;
-//import org.junit.*;
-//
-//import java.io.*;
-//import java.net.*;
-//import java.util.function.*;
-//
-///**
-//* Created by daniels on 31/03/2015.
-//*/
-//public class MapEventListenerStatelessClientTest
-//{
-//    private static final String _chronicleMapStringFilePath = "C:\\LocalFolder\\temp\\Chronicle\\chronicleMapStringListenerTest";
-//    private static File _chronicleStringMapFile;
-//
-//    private static final String _chronicleMapStringValueFilePath = "C:\\LocalFolder\\temp\\Chronicle\\chronicleMapStringValueListenerTest";
-//    private static File _chronicleStringValueMapFile;
-//
-//    private ChronicleTestEventListener _chronicleTestEventListener;
-//
-//    private ChronicleMap<String, String> _chronicleMapString;
-//    private ChronicleMap<String, String> _chronicleMapStringClient;
-//
-//    private ChronicleMap<String, StringValue> _chronicleMapStringValue;
-//    private ChronicleMap<String, StringValue> _chronicleMapStringValueClient;
-//
-//    private final String _value1 = "TestValue1";
-//    private final String _value2 = "TestValue2";
-//
-//    private static int _noOfEventsTriggered = 0;
-//
-//    @BeforeClass
-//    public static void beforeClass() throws IOException
-//    {
-//        _chronicleStringMapFile = new File(_chronicleMapStringFilePath);
-//        _chronicleStringValueMapFile = new File(_chronicleMapStringValueFilePath);
-//    }
-//
-//    @Before
-//    public void setUp() throws Exception
-//    {
-//        _noOfEventsTriggered = 0;
-//
-//        ChronicleTools.deleteDirOnExit(_chronicleMapStringFilePath);
-//        ChronicleTools.deleteDirOnExit(_chronicleMapStringValueFilePath);
-//
-//        _chronicleTestEventListener = new ChronicleTestEventListener();
-//
-//        _chronicleMapString = ChronicleMapBuilder
-//                .of(String.class, String.class)
-//                .replication((byte) 2, TcpTransportAndNetworkConfig.of(8076))
-//                .eventListener(_chronicleTestEventListener)
-//                .createPersistedTo(_chronicleStringMapFile);
-//
-//        _chronicleMapStringClient = ChronicleMapBuilder
-//                .of(String.class, String.class, new InetSocketAddress("localhost", 8076))
-//                .putReturnsNull()
-//                .create();
-//
-//        _chronicleMapStringClient = ChronicleMapStatelessClientBuilder
-//                .createClientOf(new InetSocketAddress("localhost", 8076));
-//
-////        _chronicleMapStringValue = ChronicleMapBuilder
-////                .of(String.class, StringValue.class)
-////                .replication((byte) 3, TcpTransportAndNetworkConfig.of(8078))
-////                .eventListener(_chronicleTestEventListener)
-////                .createPersistedTo(_chronicleStringValueMapFile);
-////
-////        _chronicleMapStringValueClient = ChronicleMapStatelessClientBuilder
-////                .createClientOf(new InetSocketAddress("localhost", 8078));
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception
-//    {
-//        ChronicleTools.deleteDirOnExit(_chronicleMapStringFilePath);
-//        ChronicleTools.deleteDirOnExit(_chronicleMapStringValueFilePath);
-//    }
-//
-//    /**
-//     * Test that event listener is triggered for every put.
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void testMapEvenListenerClientPut() throws Exception
-//    {
-//        String testKey = "TestKeyPut";
-//        int noOfIterations = 50;
-//
-//        testIterateAndAlternate(
-//                (x) -> _chronicleMapString.put(testKey, x),
-//                (x) -> _chronicleMapString.put(testKey, x),
-//                noOfIterations);
-//    }
-//
-//    /**
-//     * Test that event listener is triggered for every replace.
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void testMapEvenListenerReplace() throws Exception
-//    {
-//        String testKey = "TestKeyGetReplace";
-//        int noOfIterations = 50;
-//
-//        _chronicleMapString.put(testKey, _value2);
-//        _noOfEventsTriggered = 0;
-//
-//        Consumer<String> consumer = (x) -> _chronicleMapString.replace(testKey, x);
-//
-//        testIterateAndAlternate(
-//                consumer,
-//                consumer,
-//                noOfIterations);
-//    }
-//
-//    /**
-//     * Test that event listener is triggered for every "acquireUsingLocked" value update.
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void testMapEvenListenerAcquireUsingLocked() throws Exception
-//    {
-//        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
-//
-//        String testKey = "TestKeyAcquireUsingLocked";
-//        int noOfIterations = 50;
-//
-//        Consumer<String> consumer = (x) -> {
-//            try (WriteContext<String, StringValue> writeContext = _chronicleMapStringValue.acquireUsingLocked(testKey, valueInstance))
-//            {
-//                valueInstance.setValue(x);
-//            }
-//        };
-//
-//        testIterateAndAlternate(
-//                consumer,
-//                consumer,
-//                noOfIterations);
-//    }
-//
-//    /**
-//     * Test that event listener is triggered for every "acquireUsing" value update.
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void testMapEvenListenerAcquireUsing() throws Exception
-//    {
-//        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
-//
-//        String testKey = "TestKeyAcquireUsing";
-//        int noOfIterations = 50;
-//
-//        Consumer<String> consumer = (x) -> {
-//            StringValue stringValue = _chronicleMapStringValue.acquireUsing(testKey, valueInstance);
-//            stringValue.setValue(x);
-//        };
-//
-//        testIterateAndAlternate(
-//                consumer,
-//                consumer,
-//                noOfIterations);
-//    }
-//
-//    /**
-//     * Test that event listener is triggered for every "getUsing" value update.
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void testMapEvenListenerGetUsing() throws Exception
-//    {
-//        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
-//
-//        String testKey = "TestKeyGetUsing";
-//        int noOfIterations = 50;
-//
-//        _chronicleMapStringValue.put(testKey, valueInstance);
-//
-//        Consumer<String> consumer = (x) -> {
-//            StringValue using = _chronicleMapStringValue.getUsing(testKey, valueInstance);
-//            using.setValue(x);
-//        };
-//
-//        testIterateAndAlternate(
-//                consumer,
-//                consumer,
-//                noOfIterations);
-//    }
-//
-//    /**
-//     * Performs the given number of iterations and alternates between calling consumer1 and consumer2 passing
-//     * either _value1 or _value2.
-//     *
-//     * @param consumer1      Consumer1 to call.
-//     * @param consumer2      Consumer2 to call.
-//     * @param noOfIterations Number of iterations to perform.
-//     */
-//    private void testIterateAndAlternate(Consumer<String> consumer1, Consumer<String> consumer2, int noOfIterations)
-//    {
-//        long startTime = System.nanoTime();
-//
-//        for (int i = 0; i < noOfIterations; i++)
-//        {
-//            if (i % 2 == 0)
-//            {
-//                consumer1.accept(_value1);
-//            }
-//            else
-//            {
-//                consumer2.accept(_value2);
-//            }
-//        }
-//
-//        double runtime = TestUtils.calculateAndPrintRuntime(startTime);
-//
-//        //Test that 50 updates takes less than 1 second
-////        Assert.assertTrue(runtime < 1000000000);
-//
-//        Assert.assertEquals(noOfIterations, _noOfEventsTriggered);
-//    }
-//
-//    private class ChronicleTestEventListener extends MapEventListener
-//    {
-//        @Override
-//        public void onPut(Object key, Object newValue, Object replacedValue, boolean replicationEvent)
-//        {
-//            _noOfEventsTriggered++;
-//        }
-//    }
-//}
+package jp.mufg.chronicle.map.eventlistener;
+
+import jp.mufg.TestUtils;
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.engine2.Chassis;
+import net.openhft.chronicle.engine2.api.Session;
+import net.openhft.chronicle.engine2.api.TopicSubscriber;
+import net.openhft.chronicle.engine2.api.map.KeyValueStore;
+import net.openhft.chronicle.engine2.map.FilePerKeyValueStore;
+import org.junit.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import static net.openhft.chronicle.engine2.Chassis.*;
+
+/**
+ * Created by daniels on 31/03/2015.
+ */
+public class MapEventListenerStatelessClientTest {
+    private static final String _mapBasePath = OS.TMP + "/Chronicle";
+
+    private ChronicleTestEventListener _chronicleTestEventListener;
+
+    private Map<String, String> _StringStringMap;
+    private Map<String, String> _StringStringMapClient;
+
+    private final String _value1 = new String(new char[2 << 20]);//;"TestValue1";
+    private final String _value2;
+
+    private static int _noOfEventsTriggered = 0;
+
+    public MapEventListenerStatelessClientTest() {
+        char[] value = new char[2 << 20];
+        Arrays.fill(value, 'd');
+        _value2 = new String(value);
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        resetChassis();
+
+        registerFactory("", KeyValueStore.class, context -> new FilePerKeyValueStore(context.basePath(_mapBasePath)));
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        _noOfEventsTriggered = 0;
+
+        _StringStringMap = Chassis.acquireMap("chronicleMapString?putReturnsNull=true", String.class, String.class);
+        _StringStringMap.clear();
+
+        _chronicleTestEventListener = new ChronicleTestEventListener();
+
+
+        // TODO change this to be a remote session.
+        Session clientSession = defaultSession();
+        _StringStringMapClient = clientSession.acquireMap("chronicleMapString", String.class, String.class);
+        clientSession.registerTopicSubscriber("chronicleMapString", String.class, String.class, _chronicleTestEventListener);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        _StringStringMap.clear();
+        Chassis.defaultSession().close();
+    }
+
+    /**
+     * Test that event listener is triggered for every put.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMapEvenListenerClientPut() throws Exception {
+        String testKey = "TestKeyPut";
+        int noOfIterations = 50;
+
+        testIterateAndAlternate(
+                (x) -> _StringStringMap.put(testKey, x),
+                (x) -> _StringStringMap.put(testKey, x),
+                noOfIterations);
+    }
+
+    /**
+     * Test that event listener is triggered for every replace.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMapEvenListenerReplace() throws Exception {
+        String testKey = "TestKeyGetReplace";
+        int noOfIterations = 50;
+
+        _StringStringMap.put(testKey, _value2);
+        _noOfEventsTriggered = 0;
+
+        Consumer<String> consumer = (x) -> _StringStringMap.replace(testKey, x);
+
+        testIterateAndAlternate(
+                consumer,
+                consumer,
+                noOfIterations);
+    }
+
+    /**
+     * Test that event listener is triggered for every "acquireUsingLocked" value update.
+     *
+     * @throws Exception
+     */
+/*
+    @Test
+    public void testMapEvenListenerAcquireUsingLocked() throws Exception
+    {
+        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
+
+        String testKey = "TestKeyAcquireUsingLocked";
+        int noOfIterations = 50;
+
+        Consumer<String> consumer = (x) -> {
+            try (WriteContext<String, StringValue> writeContext = _chronicleMapStringValue.acquireUsingLocked(testKey, valueInstance))
+            {
+                valueInstance.setValue(x);
+            }
+        };
+
+        testIterateAndAlternate(
+                consumer,
+                consumer,
+                noOfIterations);
+    }
+*/
+
+    /**
+     * Test that event listener is triggered for every "acquireUsing" value update.
+     *
+     * @throws Exception
+     */
+/*
+    @Test
+    public void testMapEvenListenerAcquireUsing() throws Exception
+    {
+        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
+
+        String testKey = "TestKeyAcquireUsing";
+        int noOfIterations = 50;
+
+        Consumer<String> consumer = (x) -> {
+            StringValue stringValue = _chronicleMapStringValue.acquireUsing(testKey, valueInstance);
+            stringValue.setValue(x);
+        };
+
+        testIterateAndAlternate(
+                consumer,
+                consumer,
+                noOfIterations);
+    }
+*/
+
+    /**
+     * Test that event listener is triggered for every "getUsing" value update.
+     *
+     * @throws Exception
+     */
+/*
+    @Test
+    public void testMapEvenListenerGetUsing() throws Exception
+    {
+        StringValue valueInstance = _chronicleMapStringValue.newValueInstance();
+
+        String testKey = "TestKeyGetUsing";
+        int noOfIterations = 50;
+
+        _chronicleMapStringValue.put(testKey, valueInstance);
+
+        Consumer<String> consumer = (x) -> {
+            StringValue using = _chronicleMapStringValue.getUsing(testKey, valueInstance);
+            using.setValue(x);
+        };
+
+        testIterateAndAlternate(
+                consumer,
+                consumer,
+                noOfIterations);
+    }
+*/
+
+    /**
+     * Performs the given number of iterations and alternates between calling consumer1 and consumer2 passing
+     * either _value1 or _value2.
+     *
+     * @param consumer1      Consumer1 to call.
+     * @param consumer2      Consumer2 to call.
+     * @param noOfIterations Number of iterations to perform.
+     */
+    private void testIterateAndAlternate(Consumer<String> consumer1, Consumer<String> consumer2, int noOfIterations) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < noOfIterations; i++) {
+            if (i % 2 == 0) {
+                consumer1.accept(_value1);
+            } else {
+                consumer2.accept(_value2);
+            }
+        }
+
+        double runtime = TestUtils.calculateAndPrintRuntime(startTime);
+
+        //Test that 50 updates takes less than 1 second
+//        Assert.assertTrue(runtime < 1000000000);
+
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(20);
+                if (_noOfEventsTriggered == noOfIterations) break;
+            } catch (InterruptedException e) {
+                throw new AssertionError(e);
+            }
+        }
+        Assert.assertEquals(noOfIterations, _noOfEventsTriggered);
+    }
+
+    static class ChronicleTestEventListener implements TopicSubscriber<String, String> {
+        @Override
+        public void onMessage(String topic, String message) {
+            _noOfEventsTriggered++;
+        }
+    }
+}
