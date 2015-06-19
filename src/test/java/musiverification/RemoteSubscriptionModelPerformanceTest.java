0dcfc8b9299cc92fa2d8e5d9bc088ab6c622ed2f
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-19 13:22:58 +0100

    CHENT-49 Set a remote subscription test

---
+++ src/test/java/musiverification/RemoteSubscriptionModelPerformanceTest.java
@@ -0,0 +1,379 @@
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package musiverification;
+
+import ddp.api.TestUtils;
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.engine.api.map.KeyValueStore;
+import net.openhft.chronicle.engine.api.map.MapEvent;
+import net.openhft.chronicle.engine.api.map.MapEventListener;
+import net.openhft.chronicle.engine.api.map.MapView;
+import net.openhft.chronicle.engine.api.pubsub.InvalidSubscriberException;
+import net.openhft.chronicle.engine.api.pubsub.Subscriber;
+import net.openhft.chronicle.engine.api.pubsub.TopicSubscriber;
+import net.openhft.chronicle.engine.map.ChronicleMapKeyValueStore;
+import net.openhft.chronicle.engine.map.VanillaMapView;
+import net.openhft.chronicle.engine.tree.VanillaAssetTree;
+import org.junit.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+@Ignore("TODO CHENT-49")
+public class RemoteSubscriptionModelPerformanceTest {
+    public static final boolean QUICK = Boolean.getBoolean("quick");
+
+    //TODO DS test having the server side on another machine
+    private static final int _noOfPuts = 50;
+    private static final int _noOfRunsToAverage = QUICK ? 2 : 10;
+    private static final long _secondInNanos = (QUICK ? 2 : 1) * 1_000_000_000;
+    private static final AtomicInteger counter = new AtomicInteger();
+    private static String _testStringFilePath = "Vols" + File.separator + "USDVolValEnvOIS-BO.xml";
+    private static String _twoMbTestString;
+    private static int _twoMbTestStringLength;
+    private static Map<String, String> _testMap;
+    private static VanillaAssetTree serverAssetTree, clientAssetTree;
+
+    private final String _mapName = "PerfTestMap" + counter.incrementAndGet();
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        _twoMbTestString = TestUtils.loadSystemResourceFileToString(_testStringFilePath);
+        _twoMbTestStringLength = _twoMbTestString.length();
+
+        serverAssetTree = new VanillaAssetTree(1).forTesting();
+        serverAssetTree.root().addWrappingRule(MapView.class, "map directly to KeyValueStore", VanillaMapView::new, KeyValueStore.class);
+        serverAssetTree.root().addLeafRule(KeyValueStore.class, "use Chronicle Map", (context, asset) ->
+                new ChronicleMapKeyValueStore(context.basePath(OS.TARGET).entries(50).averageValueSize(2 << 20), asset));
+
+        clientAssetTree = new VanillaAssetTree().forRemoteAccess();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        Files.deleteIfExists(Paths.get(OS.TARGET, _mapName));
+
+        _testMap = clientAssetTree.acquireMap(_mapName, String.class, String.class);
+
+        _testMap.clear();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() {
+        clientAssetTree.close();
+        serverAssetTree.close();
+    }
+
+    /**
+     * Test that listening to events for a given key can handle 50 updates per second of 2 MB string values.
+     *
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testSubscriptionMapEventOnKeyPerformance() {
+        String key = TestUtils.getKey(_mapName, 0);
+
+        //Create subscriber and register
+        TestChronicleKeyEventSubscriber keyEventSubscriber = new TestChronicleKeyEventSubscriber(_twoMbTestStringLength);
+
+        clientAssetTree.registerSubscriber(_mapName + "/" + key + "?bootstrap=false", String.class, keyEventSubscriber);
+
+        //Perform test a number of times to allow the JVM to warm up, but verify runtime against average
+        TestUtils.runMultipleTimesAndVerifyAvgRuntime(() -> {
+            IntStream.range(0, _noOfPuts).forEach(i ->
+            {
+                _testMap.put(key, _twoMbTestString);
+            });
+        }, _noOfRunsToAverage, _secondInNanos);
+
+        //Test that the correct number of events was triggered on event listener
+        Assert.assertEquals(_noOfPuts * _noOfRunsToAverage, keyEventSubscriber.getNoOfEvents().get());
+
+        clientAssetTree.unregisterSubscriber(_mapName + "/" + key, keyEventSubscriber);
+    }
+
+    /**
+     * Test that listening to events for a given map can handle 50 updates per second of 2 MB string values and are
+     * triggering events which contain both the key and value (topic).
+     *
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testSubscriptionMapEventOnTopicPerformance() {
+        String key = TestUtils.getKey(_mapName, 0);
+
+        //Create subscriber and register
+        TestChronicleTopicSubscriber topicSubscriber = new TestChronicleTopicSubscriber(key, _twoMbTestStringLength);
+
+        clientAssetTree.registerTopicSubscriber(_mapName, String.class, String.class, topicSubscriber);
+
+        //Perform test a number of times to allow the JVM to warm up, but verify runtime against average
+        TestUtils.runMultipleTimesAndVerifyAvgRuntime(() -> {
+            IntStream.range(0, _noOfPuts).forEach(i ->
+            {
+                _testMap.put(key, _twoMbTestString);
+            });
+        }, _noOfRunsToAverage, _secondInNanos);
+
+        //Test that the correct number of events was triggered on event listener
+        Assert.assertEquals(_noOfPuts * _noOfRunsToAverage, topicSubscriber.getNoOfEvents().get());
+
+        clientAssetTree.unregisterTopicSubscriber(_mapName, topicSubscriber);
+    }
+
+    /**
+     * Tests the performance of an event listener on the map for Insert events of 2 MB strings.
+     * Expect it to handle at least 50 2 MB updates per second.
+     */
+    @Test
+    public void testSubscriptionMapEventListenerInsertPerformance() {
+        //Create subscriber and register
+        TestChronicleMapEventListener mapEventListener = new TestChronicleMapEventListener(_mapName, _twoMbTestStringLength);
+
+        Subscriber<MapEvent> mapEventSubscriber = e -> e.apply(mapEventListener);
+        clientAssetTree.registerSubscriber(_mapName, MapEvent.class, mapEventSubscriber);
+
+        //Perform test a number of times to allow the JVM to warm up, but verify runtime against average
+        TestUtils.runMultipleTimesAndVerifyAvgRuntime(() -> {
+            IntStream.range(0, _noOfPuts).forEach(i ->
+            {
+                _testMap.put(TestUtils.getKey(_mapName, i), _twoMbTestString);
+            });
+
+            //Test that the correct number of events were triggered on event listener
+            Assert.assertEquals(_noOfPuts, mapEventListener.getNoOfInsertEvents().get());
+            Assert.assertEquals(0, mapEventListener.getNoOfRemoveEvents().get());
+            Assert.assertEquals(0, mapEventListener.getNoOfUpdateEvents().get());
+
+            _testMap.clear();
+
+            mapEventListener.resetCounters();
+
+        }, _noOfRunsToAverage, _secondInNanos);
+        clientAssetTree.unregisterSubscriber(_mapName, mapEventSubscriber);
+    }
+
+    /**
+     * Tests the performance of an event listener on the map for Update events of 2 MB strings.
+     * Expect it to handle at least 50 2 MB updates per second.
+     */
+    @Test
+    public void testSubscriptionMapEventListenerUpdatePerformance() {
+        //Put values before testing as we want to ignore the insert events
+        Function<Integer, Object> putFunction = a -> _testMap.put(TestUtils.getKey(_mapName, a), _twoMbTestString);
+
+        IntStream.range(0, _noOfPuts).forEach(i ->
+        {
+            putFunction.apply(i);
+        });
+
+        //Create subscriber and register
+        TestChronicleMapEventListener mapEventListener = new TestChronicleMapEventListener(_mapName, _twoMbTestStringLength);
+
+        Subscriber<MapEvent> mapEventSubscriber = e -> e.apply(mapEventListener);
+        clientAssetTree.registerSubscriber(_mapName + "?bootstrap=false", MapEvent.class, mapEventSubscriber);
+
+        //Perform test a number of times to allow the JVM to warm up, but verify runtime against average
+        TestUtils.runMultipleTimesAndVerifyAvgRuntime(() -> {
+            IntStream.range(0, _noOfPuts).forEach(i ->
+            {
+                putFunction.apply(i);
+            });
+
+            //Test that the correct number of events were triggered on event listener
+            Assert.assertEquals(0, mapEventListener.getNoOfInsertEvents().get());
+            Assert.assertEquals(0, mapEventListener.getNoOfRemoveEvents().get());
+            Assert.assertEquals(_noOfPuts, mapEventListener.getNoOfUpdateEvents().get());
+
+            mapEventListener.resetCounters();
+
+        }, _noOfRunsToAverage, _secondInNanos);
+        clientAssetTree.unregisterSubscriber(_mapName, mapEventSubscriber);
+    }
+
+    /**
+     * Tests the performance of an event listener on the map for Remove events of 2 MB strings.
+     * Expect it to handle at least 50 2 MB updates per second.
+     */
+    @Test
+    public void testSubscriptionMapEventListenerRemovePerformance() {
+        //Put values before testing as we want to ignore the insert and update events
+
+        //Create subscriber and register
+        TestChronicleMapEventListener mapEventListener = new TestChronicleMapEventListener(_mapName, _twoMbTestStringLength);
+
+        Subscriber<MapEvent> mapEventSubscriber = e -> e.apply(mapEventListener);
+        clientAssetTree.registerSubscriber(_mapName + "?bootstrap=false", MapEvent.class, mapEventSubscriber);
+
+        //Perform test a number of times to allow the JVM to warm up, but verify runtime against average
+        long runtimeInNanos = 0;
+
+        for (int i = 0; i < _noOfRunsToAverage; i++) {
+            //Put values before testing as we want to ignore the insert and update events
+            IntStream.range(0, _noOfPuts).forEach(c ->
+            {
+                _testMap.put(TestUtils.getKey(_mapName, c), _twoMbTestString);
+            });
+
+            mapEventListener.resetCounters();
+
+            long startTime = System.nanoTime();
+
+            IntStream.range(0, _noOfPuts).forEach(c ->
+            {
+                _testMap.remove(TestUtils.getKey(_mapName, c));
+            });
+
+            runtimeInNanos += System.nanoTime() - startTime;
+
+            //Test that the correct number of events were triggered on event listener
+            Assert.assertEquals(0, mapEventListener.getNoOfInsertEvents().get());
+            Assert.assertEquals(_noOfPuts, mapEventListener.getNoOfRemoveEvents().get());
+            Assert.assertEquals(0, mapEventListener.getNoOfUpdateEvents().get());
+        }
+
+        Assert.assertTrue((runtimeInNanos / (_noOfPuts * _noOfRunsToAverage)) <= _secondInNanos);
+        clientAssetTree.unregisterSubscriber(_mapName, mapEventSubscriber);
+    }
+
+    /**
+     * Checks that all updates triggered are for the key specified in the constructor and increments the number of
+     * updates.
+     */
+    class TestChronicleKeyEventSubscriber implements Subscriber<String> {
+        private int _stringLength;
+        private AtomicInteger _noOfEvents = new AtomicInteger(0);
+
+        public TestChronicleKeyEventSubscriber(int stringLength) {
+            _stringLength = stringLength;
+        }
+
+        public AtomicInteger getNoOfEvents() {
+            return _noOfEvents;
+        }
+
+        @Override
+        public void onMessage(String newValue) {
+            Assert.assertEquals(_stringLength, newValue.length());
+            _noOfEvents.incrementAndGet();
+        }
+    }
+
+    /**
+     * Topic subscriber checking for each message that it is for the right key (in constructor) and the expected size
+     * value.
+     * Increments event counter which can be checked at the end of the test.
+     */
+    class TestChronicleTopicSubscriber implements TopicSubscriber<String, String> {
+        private String _keyName;
+        private int _stringLength;
+        private AtomicInteger _noOfEvents = new AtomicInteger(0);
+
+        public TestChronicleTopicSubscriber(String keyName, int stringLength) {
+            _keyName = keyName;
+            _stringLength = stringLength;
+        }
+
+        /**
+         * Test that the topic/key is the one specified in constructor and the message is the expected size.
+         *
+         * @throws InvalidSubscriberException
+         */
+        @Override
+        public void onMessage(String topic, String message) throws InvalidSubscriberException {
+            Assert.assertEquals(_keyName, topic);
+            Assert.assertEquals(_stringLength, message.length());
+
+            _noOfEvents.incrementAndGet();
+        }
+
+        public AtomicInteger getNoOfEvents() {
+            return _noOfEvents;
+        }
+    }
+
+    /**
+     * Map event listener for performance testing. Checks that the key is the one expected and the size of the value is
+     * as expected.
+     * Increments event specific counters that can be used to check agains the expected number of events.
+     */
+    class TestChronicleMapEventListener implements MapEventListener<String, String> {
+        private AtomicInteger _noOfInsertEvents = new AtomicInteger(0);
+        private AtomicInteger _noOfUpdateEvents = new AtomicInteger(0);
+        private AtomicInteger _noOfRemoveEvents = new AtomicInteger(0);
+
+        private String _mapName;
+        private int _stringLength;
+
+        public TestChronicleMapEventListener(String mapName, int stringLength) {
+            _mapName = mapName;
+            _stringLength = stringLength;
+        }
+
+        @Override
+        public void update(String key, String oldValue, String newValue) {
+            testKeyAndValue(key, newValue, _noOfUpdateEvents);
+        }
+
+        @Override
+        public void insert(String key, String value) {
+            testKeyAndValue(key, value, _noOfInsertEvents);
+        }
+
+        @Override
+        public void remove(String key, String value) {
+            testKeyAndValue(key, value, _noOfRemoveEvents);
+        }
+
+        public AtomicInteger getNoOfInsertEvents() {
+            return _noOfInsertEvents;
+        }
+
+        public AtomicInteger getNoOfUpdateEvents() {
+            return _noOfUpdateEvents;
+        }
+
+        public AtomicInteger getNoOfRemoveEvents() {
+            return _noOfRemoveEvents;
+        }
+
+        public void resetCounters() {
+            _noOfInsertEvents = new AtomicInteger(0);
+            _noOfUpdateEvents = new AtomicInteger(0);
+            _noOfRemoveEvents = new AtomicInteger(0);
+        }
+
+        private void testKeyAndValue(String key, String value, AtomicInteger counterToIncrement) {
+            int counter = counterToIncrement.getAndIncrement();
+            Assert.assertEquals(TestUtils.getKey(_mapName, counter), key);
+            Assert.assertEquals(_stringLength, value.length());
+        }
+    }
+}
