b4dc0181ebd14148c219e938a756bf08a3764bda
daniels-musi <daniel.schiermer@int.sc.mufg.jp>
2015-07-02 09:20:46 +0100

    Adding test file and example class.

---
+++ src/test/java/examples/Example1.java
@@ -0,0 +1,38 @@
+package examples;
+
+import net.openhft.chronicle.core.*;
+import net.openhft.chronicle.engine.api.map.*;
+import net.openhft.chronicle.engine.api.tree.*;
+import net.openhft.chronicle.engine.map.*;
+import net.openhft.chronicle.engine.tree.*;
+import org.junit.*;
+
+import java.util.*;
+
+public class Example1
+{
+    public static void main(String[] args)
+    {
+        AssetTree assetTree = new VanillaAssetTree(1);
+
+        //View is the interface you use for the underlying store (wrapper for underlying store)
+        //Leaf rule is not expected to be layered on top of anything, min 1 leaf rule.
+        //LEaf is the base for whatever implementation you want to use
+        assetTree.root().addLeafRule(AuthenticatedKeyValueStore.class, "My auth store", VanillaKeyValueStore::new);
+        Asset testAsset = assetTree.acquireAsset(RequestContext.requestContext("/test"));
+        testAsset.addLeafRule(AuthenticatedKeyValueStore.class, "My auth store", (rc, asset) -> new FilePerKeyValueStore(rc.basePath("C:\\LocalFolder\\"), asset));
+
+        //Defines a view on an already existing service - a leaf rule has to exist for any wrapping rule
+        assetTree.root().addWrappingRule(MapView.class, "My map view", VanillaMapView::new, AuthenticatedKeyValueStore.class);
+
+        Map<String, String> map1 = assetTree.acquireMap("map1", String.class, String.class);
+
+        map1.put("Key1", "Value1");
+        System.out.println(map1.get("Key1"));
+        Assert.assertEquals(VanillaKeyValueStore.class, ((MapView) map1).underlying().getClass());
+
+        Map<String, String> test_map = assetTree.acquireMap("/test2/map1", String.class, String.class);
+        Assert.assertEquals(FilePerKeyValueStore.class, ((MapView) test_map).underlying().getClass());
+
+    }
+}
