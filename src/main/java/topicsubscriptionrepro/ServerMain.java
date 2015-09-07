f7f0c3a307632891525dfa0fdb31a2e921a40cf7
danielshaya <daniel@familyshaya.com>
2015-09-07 16:59:25 +0100

    CE-25 Basic implementation

--- src/main/java/topicsubscriptionrepro/ServerMain.java
+++ src/main/java/topicsubscriptionrepro/ServerMain.java
@@ -15,7 +15,7 @@ public class ServerMain
 
     public static void main(String[] args) throws IOException
     {
-        YamlLogging.setAll(true);
+//        YamlLogging.setAll(true);
         VanillaAssetTree assetTree = new VanillaAssetTree().forServer();
 //        VanillaAssetTree assetTree = new VanillaAssetTree().forTesting();
 
@@ -25,20 +25,20 @@ public class ServerMain
 
         //FIXME remove these rules and it still won't work.
         //Add wrapping rules for authorization and authentication
-        root.addWrappingRule(DdpAuthorizationKeyValueStore.class, "DDP authorization store",
-                DdpAuthorizationKeyValueStore::new, SubscriptionKeyValueStore.class);
-
-        root.addWrappingRule(AuthenticatedKeyValueStore.class, "DDP authentication store",
-                DdpAuthenticationKeyValueStore::new, DdpAuthorizationKeyValueStore.class);
-
-        //Add wrapping and leaf rules for authorization authentication subscription
-        root.addWrappingRule(DdpAuthorizationKeyValueSubscription.class, "DDP authorization subscription",
-                DdpAuthorizationKeyValueSubscription::new, VanillaKVSSubscription.class);
-
-        root.addWrappingRule(ObjectKVSSubscription.class, "DDP authentication subscription",
-                DdpAuthenticationKeyValueSubscription::new, DdpAuthorizationKeyValueSubscription.class);
-
-        root.addLeafRule(VanillaKVSSubscription.class, "Chronicle vanilla subscription", VanillaKVSSubscription::new);
+//        root.addWrappingRule(DdpAuthorizationKeyValueStore.class, "DDP authorization store",
+//                DdpAuthorizationKeyValueStore::new, SubscriptionKeyValueStore.class);
+//
+//        root.addWrappingRule(AuthenticatedKeyValueStore.class, "DDP authentication store",
+//                DdpAuthenticationKeyValueStore::new, DdpAuthorizationKeyValueStore.class);
+//
+//        //Add wrapping and leaf rules for authorization authentication subscription
+//        root.addWrappingRule(DdpAuthorizationKeyValueSubscription.class, "DDP authorization subscription",
+//                DdpAuthorizationKeyValueSubscription::new, VanillaKVSSubscription.class);
+//
+//        root.addWrappingRule(ObjectKVSSubscription.class, "DDP authentication subscription",
+//                DdpAuthenticationKeyValueSubscription::new, DdpAuthorizationKeyValueSubscription.class);
+//
+//        root.addLeafRule(VanillaKVSSubscription.class, "Chronicle vanilla subscription", VanillaKVSSubscription::new);
 
         System.out.println("Press any key to exit...");
         System.in.read();
