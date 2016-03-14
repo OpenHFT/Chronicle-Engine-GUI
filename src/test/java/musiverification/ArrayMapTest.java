516ebba201388af97b5c194163a4324d451d4107
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-03-14 12:42:28 +0000

    removed warnings and improved chronicle queue replication

--- src/test/java/musiverification/ArrayMapTest.java
+++ src/test/java/musiverification/ArrayMapTest.java
@@ -70,7 +70,7 @@ public class ArrayMapTest {
     @After
     public void afterMethod() {
         final Throwable th = t.getAndSet(null);
-        if (th != null) Jvm.rethrow(th);
+        if (th != null) throw Jvm.rethrow(th);
     }
 
     @Before
