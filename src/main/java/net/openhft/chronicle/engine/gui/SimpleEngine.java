45a1590be30dd1cb11000a330c3325a0c727ae7d
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-10-18 17:59:01 +0100

    now computing volumn outside method

--- src/main/java/net/openhft/chronicle/engine/gui/SimpleEngine.java
+++ src/main/java/net/openhft/chronicle/engine/gui/SimpleEngine.java
@@ -31,14 +31,12 @@ public class SimpleEngine {
         double adjClose;
 
         public MarketData(double open, double high, double low, double close,
-                          double volumnMillions,
-                          double volumeThousands,
-                          double volume, double adjClose) {
+                          double adjClose, final double v) {
             this.open = open;
             this.high = high;
             this.low = low;
             this.close = close;
-            this.volume = volume + (volumeThousands * 1000) + (volumnMillions * 1_000_000L);
+            this.volume = v;
             this.adjClose = adjClose;
         }
     }
@@ -63,57 +61,57 @@ public class SimpleEngine {
         {
             MapView<Date, MarketData> map = assetTree.acquireMap("/shares/APPL", Date.class,
                     MarketData
-                    .class);
+                            .class);
 
             SimpleDateFormat sd = new SimpleDateFormat("dd MMM yyyy");
 
             try {
-                map.put(sd.parse("7 Oct 2016"), new MarketData(114.31, 114.56, 113.51, 114.06, 24, 358, 400, 114.06));
-                map.put(sd.parse("6 Oct 2016"), new MarketData(113.70, 114.34, 113.13, 113.89, 28, 779, 300, 113.89));
-                map.put(sd.parse("5 Oct 2016"), new MarketData(113.40, 113.66, 112.69, 113.05, 21, 453, 100, 113.05));
-                map.put(sd.parse("4 Oct 2016"), new MarketData(113.06, 114.31, 112.63, 113.00, 29, 736, 800, 113.00));
-                map.put(sd.parse("3 Oct 2016"), new MarketData(112.71, 113.05, 112.28, 112.52, 21, 701, 800, 112.52));
-                map.put(sd.parse("30 Sep 2016"), new MarketData(112.46, 113.37, 111.80, 113.05, 36, 379, 100, 113.05));
-                map.put(sd.parse("29 Sep 2016"), new MarketData(113.16, 113.80, 111.80, 112.18, 35, 887, 000, 112.18));
-                map.put(sd.parse("28 Sep 2016"), new MarketData(113.69, 114.64, 113.43, 113.95, 29, 641, 100, 113.95));
-                map.put(sd.parse("27 Sep 2016"), new MarketData(113.00, 113.18, 112.34, 113.09, 24, 607, 400, 113.09));
-                map.put(sd.parse("26 Sep 2016"), new MarketData(111.64, 113.39, 111.55, 112.88, 29, 869, 400, 112.88));
-                map.put(sd.parse("23 Sep 2016"), new MarketData(114.42, 114.79, 111.55, 112.71, 52, 481, 200, 112.71));
-                map.put(sd.parse("22 Sep 2016"), new MarketData(114.35, 114.94, 114.00, 114.62, 31, 74, 000, 114.62));
-                map.put(sd.parse("21 Sep 2016"), new MarketData(113.85, 113.99, 112.44, 113.55, 36, 3, 200, 113.55));
-                map.put(sd.parse("20 Sep 2016"), new MarketData(113.05, 114.12, 112.51, 113.57, 34, 514, 300, 113.57));
-                map.put(sd.parse("19 Sep 2016"), new MarketData(115.19, 116.18, 113.25, 113.58, 47, 23, 000, 113.58));
-                map.put(sd.parse("16 Sep 2016"), new MarketData(115.12, 116.13, 114.04, 114.92, 79, 886, 900, 114.92));
-                map.put(sd.parse("15 Sep 2016"), new MarketData(113.86, 115.73, 113.49, 115.57, 89, 983, 600, 115.57));
-                map.put(sd.parse("14 Sep 2016"), new MarketData(108.73, 113.03, 108.60, 111.77, 110, 888, 700, 111.77));
-                map.put(sd.parse("13 Sep 2016"), new MarketData(107.51, 108.79, 107.24, 107.95, 62, 176, 200, 107.95));
-                map.put(sd.parse("12 Sep 2016"), new MarketData(102.65, 105.72, 102.53, 105.44, 45, 292, 800, 105.44));
-                map.put(sd.parse("9 Sep 2016"), new MarketData(104.64, 105.72, 103.13, 103.13, 46, 557, 000, 103.13));
-                map.put(sd.parse("8 Sep 2016"), new MarketData(107.25, 107.27, 105.24, 105.52, 53, 2, 000, 105.52));
-                map.put(sd.parse("7 Sep 2016"), new MarketData(107.83, 108.76, 107.07, 108.36, 42, 364, 300, 108.36));
-                map.put(sd.parse("6 Sep 2016"), new MarketData(107.90, 108.30, 107.51, 107.70, 26, 880, 400, 107.70));
-                map.put(sd.parse("2 Sep 2016"), new MarketData(107.70, 108.00, 106.82, 107.73, 26, 802, 500, 107.73));
-                map.put(sd.parse("1 Sep 2016"), new MarketData(106.14, 106.80, 105.62, 106.73, 26, 701, 500, 106.73));
-                map.put(sd.parse("31 Aug 2016"), new MarketData(105.66, 106.57, 105.64, 106.10, 29, 662, 400, 106.10));
-                map.put(sd.parse("30 Aug 2016"), new MarketData(105.80, 106.50, 105.50, 106.00, 24, 863, 900, 106.00));
-                map.put(sd.parse("29 Aug 2016"), new MarketData(106.62, 107.44, 106.29, 106.82, 24, 970, 300, 106.82));
-                map.put(sd.parse("26 Aug 2016"), new MarketData(107.41, 107.95, 106.31, 106.94, 27, 766, 300, 106.94));
-                map.put(sd.parse("25 Aug 2016"), new MarketData(107.39, 107.88, 106.68, 107.57, 25, 86, 200, 107.57));
-                map.put(sd.parse("24 Aug 2016"), new MarketData(108.57, 108.75, 107.68, 108.03, 23, 675, 100, 108.03));
-                map.put(sd.parse("23 Aug 2016"), new MarketData(108.59, 109.32, 108.53, 108.85, 21, 257, 700, 108.85));
-                map.put(sd.parse("22 Aug 2016"), new MarketData(108.86, 109.10, 107.85, 108.51, 25, 820, 200, 108.51));
-                map.put(sd.parse("19 Aug 2016"), new MarketData(108.77, 109.69, 108.36, 109.36, 25, 368, 100, 109.36));
-                map.put(sd.parse("18 Aug 2016"), new MarketData(109.23, 109.60, 109.02, 109.08, 21, 984, 700, 109.08));
-                map.put(sd.parse("17 Aug 2016"), new MarketData(109.10, 109.37, 108.34, 109.22, 25, 356, 000, 109.22));
-                map.put(sd.parse("16 Aug 2016"), new MarketData(109.63, 110.23, 109.21, 109.38, 33, 794, 400, 109.38));
-                map.put(sd.parse("15 Aug 2016"), new MarketData(108.14, 109.54, 108.08, 109.48, 25, 868, 200, 109.48));
-                map.put(sd.parse("12 Aug 2016"), new MarketData(107.78, 108.44, 107.78, 108.18, 18, 660, 400, 108.18));
-                map.put(sd.parse("11 Aug 2016"), new MarketData(108.52, 108.93, 107.85, 107.93, 27, 484, 500, 107.93));
-                map.put(sd.parse("10 Aug 2016"), new MarketData(108.71, 108.90, 107.76, 108.00, 24, 8, 500, 108.00));
-                map.put(sd.parse("9 Aug 2016"), new MarketData(108.23, 108.94, 108.01, 108.81, 26, 315, 200, 108.81));
-                map.put(sd.parse("8 Aug 2016"), new MarketData(107.52, 108.37, 107.16, 108.37, 28, 37, 200, 108.37));
-                map.put(sd.parse("5 Aug 2016"), new MarketData(106.27, 107.65, 106.18, 107.48, 40, 553, 400, 107.48));
-                map.put(sd.parse("4 Aug 2016"), new MarketData(105.58, 106.00, 105.28, 105.87, 27, 408, 700, 105.87));
+                map.put(sd.parse("7 Oct 2016"), new MarketData(114.31, 114.56, 113.51, 114.06, 114.06, 24358400L));
+                map.put(sd.parse("6 Oct 2016"), new MarketData(113.70, 114.34, 113.13, 113.89, 113.89, 28779300L));
+                map.put(sd.parse("5 Oct 2016"), new MarketData(113.40, 113.66, 112.69, 113.05, 113.05, 21453100L));
+                map.put(sd.parse("4 Oct 2016"), new MarketData(113.06, 114.31, 112.63, 113.00, 113.00, 29736800L));
+                map.put(sd.parse("3 Oct 2016"), new MarketData(112.71, 113.05, 112.28, 112.52, 112.52, 21701800L));
+                map.put(sd.parse("30 Sep 2016"), new MarketData(112.46, 113.37, 111.80, 113.05, 113.05, 36379100L));
+                map.put(sd.parse("29 Sep 2016"), new MarketData(113.16, 113.80, 111.80, 112.18, 112.18, 35887000L));
+                map.put(sd.parse("28 Sep 2016"), new MarketData(113.69, 114.64, 113.43, 113.95, 113.95, 29641100L));
+                map.put(sd.parse("27 Sep 2016"), new MarketData(113.00, 113.18, 112.34, 113.09, 113.09, 24607400L));
+                map.put(sd.parse("26 Sep 2016"), new MarketData(111.64, 113.39, 111.55, 112.88, 112.88, 29869400L));
+                map.put(sd.parse("23 Sep 2016"), new MarketData(114.42, 114.79, 111.55, 112.71, 112.71, 52481200L));
+                map.put(sd.parse("22 Sep 2016"), new MarketData(114.35, 114.94, 114.00, 114.62, 114.62, 31074000L));
+                map.put(sd.parse("21 Sep 2016"), new MarketData(113.85, 113.99, 112.44, 113.55, 113.55, 36003200L));
+                map.put(sd.parse("20 Sep 2016"), new MarketData(113.05, 114.12, 112.51, 113.57, 113.57, 34514300L));
+                map.put(sd.parse("19 Sep 2016"), new MarketData(115.19, 116.18, 113.25, 113.58, 113.58, 47023000L));
+                map.put(sd.parse("16 Sep 2016"), new MarketData(115.12, 116.13, 114.04, 114.92, 114.92, 79886900L));
+                map.put(sd.parse("15 Sep 2016"), new MarketData(113.86, 115.73, 113.49, 115.57, 115.57, 89983600L));
+                map.put(sd.parse("14 Sep 2016"), new MarketData(108.73, 113.03, 108.60, 111.77, 111.77, 110888700L));
+                map.put(sd.parse("13 Sep 2016"), new MarketData(107.51, 108.79, 107.24, 107.95, 107.95, 62176200L));
+                map.put(sd.parse("12 Sep 2016"), new MarketData(102.65, 105.72, 102.53, 105.44, 105.44, 45292800L));
+                map.put(sd.parse("9 Sep 2016"), new MarketData(104.64, 105.72, 103.13, 103.13, 103.13, 46557000L));
+                map.put(sd.parse("8 Sep 2016"), new MarketData(107.25, 107.27, 105.24, 105.52, 105.52, 53002000L));
+                map.put(sd.parse("7 Sep 2016"), new MarketData(107.83, 108.76, 107.07, 108.36, 108.36, 42364300L));
+                map.put(sd.parse("6 Sep 2016"), new MarketData(107.90, 108.30, 107.51, 107.70, 107.70, 26880400L));
+                map.put(sd.parse("2 Sep 2016"), new MarketData(107.70, 108.00, 106.82, 107.73, 107.73, 26802500L));
+                map.put(sd.parse("1 Sep 2016"), new MarketData(106.14, 106.80, 105.62, 106.73, 106.73, 26701500L));
+                map.put(sd.parse("31 Aug 2016"), new MarketData(105.66, 106.57, 105.64, 106.10, 106.10, 29662400L));
+                map.put(sd.parse("30 Aug 2016"), new MarketData(105.80, 106.50, 105.50, 106.00, 106.00, 24863900L));
+                map.put(sd.parse("29 Aug 2016"), new MarketData(106.62, 107.44, 106.29, 106.82, 106.82, 24970300L));
+                map.put(sd.parse("26 Aug 2016"), new MarketData(107.41, 107.95, 106.31, 106.94, 106.94, 27766300L));
+                map.put(sd.parse("25 Aug 2016"), new MarketData(107.39, 107.88, 106.68, 107.57, 107.57, 25086200L));
+                map.put(sd.parse("24 Aug 2016"), new MarketData(108.57, 108.75, 107.68, 108.03, 108.03, 23675100L));
+                map.put(sd.parse("23 Aug 2016"), new MarketData(108.59, 109.32, 108.53, 108.85, 108.85, 21257700L));
+                map.put(sd.parse("22 Aug 2016"), new MarketData(108.86, 109.10, 107.85, 108.51, 108.51, 25820200L));
+                map.put(sd.parse("19 Aug 2016"), new MarketData(108.77, 109.69, 108.36, 109.36, 109.36, 25368100L));
+                map.put(sd.parse("18 Aug 2016"), new MarketData(109.23, 109.60, 109.02, 109.08, 109.08, 21984700L));
+                map.put(sd.parse("17 Aug 2016"), new MarketData(109.10, 109.37, 108.34, 109.22, 109.22, 25356000L));
+                map.put(sd.parse("16 Aug 2016"), new MarketData(109.63, 110.23, 109.21, 109.38, 109.38, 33794400L));
+                map.put(sd.parse("15 Aug 2016"), new MarketData(108.14, 109.54, 108.08, 109.48, 109.48, 25868200L));
+                map.put(sd.parse("12 Aug 2016"), new MarketData(107.78, 108.44, 107.78, 108.18, 108.18, 18660400L));
+                map.put(sd.parse("11 Aug 2016"), new MarketData(108.52, 108.93, 107.85, 107.93, 107.93, 27484500L));
+                map.put(sd.parse("10 Aug 2016"), new MarketData(108.71, 108.90, 107.76, 108.00, 108.00, 24008500L));
+                map.put(sd.parse("9 Aug 2016"), new MarketData(108.23, 108.94, 108.01, 108.81, 108.81, 26315200L));
+                map.put(sd.parse("8 Aug 2016"), new MarketData(107.52, 108.37, 107.16, 108.37, 108.37, 28037200L));
+                map.put(sd.parse("5 Aug 2016"), new MarketData(106.27, 107.65, 106.18, 107.48, 107.48, 40553400L));
+                map.put(sd.parse("4 Aug 2016"), new MarketData(105.58, 106.00, 105.28, 105.87, 105.87, 27408700L));
             } catch (Exception e) {
                 e.printStackTrace();
             }
